fd9fc2462d7b82ac3a2dfcaa40ef3dd379361348
Dmalkov1993 <Dmalkov1993@yandex.ru>
2019-08-22 00:21:21 +0500

    Забыл try-catch для выдачи правильного сообщения.

--- DirectoryService/DirectoryService/Controllers/AppStartController.cs
+++ DirectoryService/DirectoryService/Controllers/AppStartController.cs
@@ -11,12 +11,19 @@
         [HttpGet]
         public string Get()
         {
-            // Заодно проверим чтение справочников из псевдо-БД
-            var версииДанных = DataLoader.ПолучитьДанныеСправочникаВерсииДанных();
-            var объектыСтроительства = DataLoader.ПолучитьДанныеСправочникаОбъектыСтроительства();
+            try
+            {
+                // Заодно проверим чтение справочников из псевдо-БД
+                var версииДанных = DataLoader.ПолучитьДанныеСправочникаВерсииДанных();
+                var объектыСтроительства = DataLoader.ПолучитьДанныеСправочникаОбъектыСтроительства();
 
-            // Если не упали - говорим Ура.
-            return $"App start successfully! Total readed objects: Data versions: {версииДанных.Count} Construction objects: {объектыСтроительства.Count}";
+                // Если не упали - говорим Ура.
+                return $"App start successfully! Total readed objects: Data versions: {версииДанных.Count} Construction objects: {объектыСтроительства.Count}";
+            }
+            catch
+            {
+                return "Произошла ошибка при чтении справочников.";
+            }
         }
     }
 }
